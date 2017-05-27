3385d3c48cfcce7543bfe36305c100df7fb28f2e
Huan Jiang <seraphjiang@gmail.com>
2017-05-27 16:38:16 -0700

    find max nums from k size sliding window by Deque(LinkedList)

---
+++ LeetCode.Problems/SlidingWindow/SlidingWindowMaximum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.SlidingWindow
+{
+    public class SlidingWindowMaximum
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0 || k <= 0) return new int[0];
+
+            var res = new int[nums.Length - k + 1];
+            var deque = new LinkedList<int>();
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                while (deque.Any() && deque.First() < i - k + 1)
+                {
+                    deque.RemoveFirst();
+                }
+
+                while(deque.Any() && nums[deque.Last()] < nums[i])
+                {
+                    deque.RemoveLast();
+                }
+
+                deque.AddLast(i);
+
+                if (i >= k - 1)
+                {
+                    res[i - k + 1] = nums[deque.First()]; 
+                }
+            }
+
+            return res;
+        }
+    }
+}
