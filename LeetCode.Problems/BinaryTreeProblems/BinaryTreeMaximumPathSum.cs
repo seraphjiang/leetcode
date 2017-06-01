a6f7c2e75966f55603aebddf4a0c3750f3e8469f
Huan Jiang <seraphjiang@gmail.com>
2017-06-01 11:33:57 -0700

    max path sum for binary tree

---
+++ LeetCode.Problems/BinaryTreeProblems/BinaryTreeMaximumPathSum.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.BinaryTreeProblems
+{
+    public class BinaryTreeMaximumPathSum
+    {
+        int max = int.MinValue;
+        public int MaxPathSum(TreeNode root)
+        {
+            Dfs(root);
+            return max;
+        }
+
+        public int Dfs(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                max = Math.Max(max, root.val);
+                return root.val;
+            }
+
+            var l = Dfs(root.left);
+            var r = Dfs(root.right);
+
+            var m = (root.left == null || root.right == null) ? (l + r) : Math.Max(l, r); // could be zero or negetive
+            max = Math.Max(max, m);
+            max = Math.Max(max, m + root.val);
+            max = Math.Max(max, root.val);
+            max = Math.Max(max, l + r + root.val);
+
+            return Math.Max(root.val, m + root.val);// must include root.
+        }
+    }
+}
---
+++ LeetCode.Tests/BinaryTreeProblemsTests/BinaryTreeMaximumPathSumTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.BinaryTreeProblems;
+using LeetCode.DataStructure.Standalone.BinaryTrees;
+
+namespace LeetCode.Tests.BinaryTreeProblemsTests
+{
+    [TestClass]
+    public class BinaryTreeMaximumPathSumTests
+    {
+        [TestMethod]
+        public void Test_BinaryTreeMaximumPathSum1()
+        {
+            var tn = (new BinaryTree()).deserialize("1,-2,-3,1,3,-2,#,-1");
+            var o = new BinaryTreeMaximumPathSum();
+            Assert.AreEqual(3, o.MaxPathSum(tn));
+        }
+        [TestMethod]
+        public void Test_BinaryTreeMaximumPathSum2()
+        {
+            var tn = (new BinaryTree()).deserialize("-1,5,#,4,#,#,2,-4");
+            var o = new BinaryTreeMaximumPathSum();
+            Assert.AreEqual(11, o.MaxPathSum(tn));
+        }
+    }
+}
