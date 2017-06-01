221a83c8c4b88516f1e7f5742352ce2728b3e46b
Huan Jiang <seraphjiang@gmail.com>
2017-06-01 14:05:15 -0700

    find fisrt missing num

---
+++ LeetCode.Problems/ArrayProblems/FindFirstMissingPositive.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.ArrayProblems
+{
+    public class FindFirstMissingPositive
+    {
+        public int FirstMissingPositiveWithSpace(int[] nums)
+        {
+            var target = new int[nums.Length];
+            foreach (var n in nums)
+            {
+                if (n < 1 || n > nums.Length) continue;
+                target[n - 1] = n;
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (target[i] != i + 1) return i + 1;
+            }
+
+            return nums.Length + 1;
+        }
+
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                while ((nums[i] > 0) && (nums[i] <= nums.Length) && (nums[i] != nums[nums[i] - 1]))
+                {
+                    /*below is wrong way to swap, because nums[i] is not invariable 
+                    var temp = nums[i];
+                    nums[i] = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = temp;
+                    */
+                    var temp = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = temp;
+
+                    Console.WriteLine("{0},{1}", nums[i], nums[nums[i] - 1]);
+                }
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1) return i + 1;
+            }
+
+            return nums.Length + 1;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArrayProblemsTests/FindFirstMissingPositiveTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.ArrayProblems;
+
+namespace LeetCode.Tests.ArrayProblemsTests
+{
+    [TestClass]
+    public class FindFirstMissingPositiveTests
+    {
+        [TestMethod]
+        public void Test_FindFirstMissingPositive1()
+        {
+            var input = new int[] { 1, 1 };
+            var o = new FindFirstMissingPositive();
+            Assert.AreEqual(2, o.FirstMissingPositive(input));
+
+            Assert.AreEqual(1, o.FirstMissingPositive(new int[] { 0}));
+
+            Assert.AreEqual(3, o.FirstMissingPositive(new int[] { 2,1 }));
+
+        }
+    }
+}
