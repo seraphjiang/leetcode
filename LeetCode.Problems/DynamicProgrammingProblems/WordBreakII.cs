661d214a48c92d32abb2659f9ba613149eccb3c0
Huan Jiang <seraphjiang@gmail.com>
2017-06-02 18:19:29 -0700

    DFS backtracking solution for WorkbreakII with pruning, AC

---
+++ LeetCode.Problems/DynamicProgrammingProblems/WordBreakII.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.DynamicProgrammingProblems
+{
+    public class WordBreakII
+    {
+        Dictionary<int, IList<string>> cache = new Dictionary<int, IList<string>>();
+        HashSet<string> dict;
+        IList<string> result;
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            dict = new HashSet<string>(wordDict);
+            result = new List<string>();
+
+            return Dfs(s, 0);
+        }
+
+        public IList<string> Dfs(string s, int start)
+        {
+            if (cache.ContainsKey(start)) return cache[start];
+
+            var res = new List<string>();
+            if(dict.Contains(s.Substring(start))) {
+                res.Add(s.Substring(start));
+            }
+            //if (cache.ContainsKey(start)) return cache[start];
+            for (var i = start+1; i < s.Length; ++i)
+            {
+                var left = s.Substring(start, i - start);
+                var right = s.Substring(i);
+                if (!dict.Contains(left)) continue;
+
+                var list = new List<string>();
+                var rights = Dfs(s, i);
+                if (rights.Count > 0)
+                {
+                    foreach (var r in rights)
+                    {
+                        res.Add(left + " " + r);
+                    }
+                }
+            }
+            cache[start] = res;
+            return res;
+        }
+    }
+}
---
+++ LeetCode.Tests/DPProblemsTests/WordBreakIITests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.DynamicProgrammingProblems;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.DPProblemsTests
+{
+    [TestClass]
+    public class WordBreakIITests
+    {
+        [TestMethod]
+        public void Test_WordBreakII1()
+        {
+            var o = new WordBreakII();
+            var expected = new List<string> { "cats and dog", "cat sand dog" };
+            var actual = o.WordBreak("catsanddog", new List<string> { "cat", "cats", "and", "sand", "dog" });
+            Assert.IsTrue(AreTheSameIgnoringOrder(expected, actual));
+        }
+        public static bool AreTheSameIgnoringOrder(IList<string> x, IList<string> y)
+        {
+            return x.Count() == y.Count()
+                && !x.Except(y).Any()
+                && !y.Except(x).Any();
+        }
+
+        [TestMethod]
+        public void Test_WordBreakII2()
+        {
+            var o = new WordBreakII();
+            var expected = new List<string> {  };
+            var actual = o.WordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", new List<string> { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" });
+            Assert.IsTrue(AreTheSameIgnoringOrder(expected, actual));
+        }
+    }
+}
