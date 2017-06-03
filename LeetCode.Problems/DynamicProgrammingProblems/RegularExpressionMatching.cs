a049c05d1cc71ad772af92463e3ed9d15587af11
Huan Jiang <seraphjiang@gmail.com>
2017-06-03 15:06:06 -0700

    Regular Expression Mathing DFS solution

---
+++ LeetCode.Problems/DynamicProgrammingProblems/RegularExpressionMatching.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.DynamicProgrammingProblems
+{
+    public class RegularExpressionMatching
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return Dfs(s, p, 0, 0);
+        }
+        public bool IsMatchBackTracking(string s, string p)
+        {
+            return Dfs(s, p, 0, 0);
+        }
+
+        /// <summary>
+        /// even it is an AC solution, there lots of redundant check code.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <param name="l"></param>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public bool Dfs(string s, string p, int l, int r)
+        {
+            if (l < s.Length && r < p.Length)
+            {
+                if (s[l] == p[r] || p[r] == '.')
+                {
+                    if (r + 1 < p.Length && p[r + 1] == '*')
+                    {
+                        while (l < s.Length && (s[l] == p[r] || p[r] == '.'))
+                        {
+                            if (Dfs(s, p, l, r + 2)) return true;
+                            l++;
+                        }
+                        return (Dfs(s, p, l, r + 2));
+                    }
+                    else
+                    {
+                        return Dfs(s, p, l + 1, r + 1);
+                    }
+                }
+                else if (p[r] == '*' && (r - 1 >= 0) && p[r - 1] == s[l])
+                {
+                    while (l < s.Length && p[r - 1] == s[l])
+                    {
+                        if (Dfs(s, p, l, r + 1)) return true; // cover "aaa", "a*a"
+                        l++;
+                    }
+                    return Dfs(s, p, l, r + 1);
+                }
+                else if (p[r] == '*' && (r - 1 >= 0) && p[r - 1] == '.') // match .*
+                {
+                    for (var i = l; i <= s.Length; i++)
+                    {
+                        if (Dfs(s, p, i, r + 1)) return true;
+                    }
+                    return false;
+                }
+                else if (p[r] == '*')
+                {
+                    return Dfs(s, p, l, r + 1);
+                }
+                else if (r + 1 < p.Length && p[r + 1] == '*')
+                {
+                    return Dfs(s, p, l, r + 2);
+                }
+            }
+
+            while (r < p.Length)
+            {
+                if (p[r] == '*') r++;
+                else if (r + 1 < p.Length && p[r + 1] == '*')
+                {
+                    r++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return l == s.Length && r == p.Length;
+        }
+    }
+}
---
+++ LeetCode.Tests/DPProblemsTests/RegularExpressionMatchingTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.DynamicProgrammingProblems;
+
+namespace LeetCode.Tests.DPProblemsTests
+{
+    [TestClass]
+    public class RegularExpressionMatchingTests
+    {
+        [TestMethod]
+        public void Test_RegularExpressionMatching1()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsFalse(o.IsMatchBackTracking("aa", "a"));
+            Assert.IsTrue(o.IsMatchBackTracking("aa", "aa"));
+            Assert.IsFalse(o.IsMatchBackTracking("aaa", "aa"));
+            Assert.IsTrue(o.IsMatchBackTracking("aa", "a*"));
+            Assert.IsTrue(o.IsMatchBackTracking("aa", ".*"));
+            Assert.IsTrue(o.IsMatchBackTracking("ab", ".*"));
+            Assert.IsTrue(o.IsMatchBackTracking("aab", "c*a*b"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching2()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("aaa", "a*a"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching3()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("aaa", "ab*a*c*a"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching4()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("aaca", "ab*a*c*a"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching5()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("a", "ab*"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching6()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("ab", ".*c"));
+        }
+        [TestMethod]
+        public void Test_RegularExpressionMatching7()
+        {
+            var o = new RegularExpressionMatching();
+            Assert.IsTrue(o.IsMatchBackTracking("bbbba", ".*a*a"));
+        }
+    }
+}
