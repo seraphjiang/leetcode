fa20a7c885385398b77183a820a49353a188ed9c
Huan Jiang <seraphjiang@gmail.com>
2017-06-05 17:51:58 -0700

    Substring with concatenation of all words

---
+++ LeetCode.Problems/StringProblems/SubstringwithConcatenationofAllWords.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.StringProblems
+{
+    public class SubstringwithConcatenationofAllWords
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var w in words)
+            {
+                if (!dict.ContainsKey(w)) dict.Add(w, 0);
+                dict[w]++;
+            }
+            var res = new List<int>();
+            var wl = words[0].Length;
+            var total = wl * words.Length;
+            var n = s.Length;
+            for (var i = 0; i < wl; ++i) // just iterate all chars is enough for all combination
+            {
+                var left = i; var count = 0; var seen = new Dictionary<string, int>();
+                for (var j = i; j + wl <= n; j += wl)
+                {
+                    var sub = s.Substring(j, wl);
+                    if (dict.ContainsKey(sub))
+                    {
+                        if (!seen.ContainsKey(sub)) seen[sub] = 0;
+                        seen[sub]++;
+
+                        if (seen[sub] <= dict[sub])
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            // 1 more sub add. need to remove word from head, till sub count match again
+                            while (seen[sub] > dict[sub])
+                            {
+                                var leftword = s.Substring(left, wl);
+                                seen[leftword]--; //since we remove word, maintain the new seen count in window
+                                if (seen[leftword] < dict[leftword]) count--; // maintain the total seen.
+                                left += wl; //pop out left head word
+                            }
+                        }
+
+                        if (count == words.Length)
+                        {
+                            res.Add(left);
+                            // make it invalid , no need to remove all, just one from left head
+                            var leftword = s.Substring(left, wl);
+                            seen[leftword]--;
+                            count--;
+                            left += wl;
+                        }
+                    }
+                    else
+                    {
+                        left = j + wl;// reset from next word since current work doesn't match
+                        count = 0;
+                        seen.Clear();//reset the search
+                    }
+                }
+            }
+
+            return res;
+        }
+        public IList<int> FindSubstringDfsTLE(string s, string[] words)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var w in words)
+            {
+                if (!dict.ContainsKey(w)) dict.Add(w, 0);
+                dict[w]++;
+            }
+            var res = new List<int>();
+            Dfs(s, dict, 0, res, 0, words[0].Length, words.Length);
+
+            return res;
+        }
+
+        public void Dfs(string s, Dictionary<string, int> words, int start, IList<int> res, int count, int len, int total)
+        {
+            if (start + len > s.Length) return;
+
+            var sub = s.Substring(start, len);
+            if (words.ContainsKey(sub) && words[sub] > 0)
+            {
+                count++;
+                if (count == total)
+                {
+                    res.Add(start - (len * (total - 1)));
+                }
+                else
+                {
+                    words[sub]--;
+                    Dfs(s, words, start + len, res, count, len, total);
+                    words[sub]++;
+                }
+            }
+
+            Dfs(s, words, start + 1, res, 0, len, total);
+        }
+    }
+}
---
+++ LeetCode.Tests/StringProblemsTests/SubstringwithConcatenationofAllWordsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.StringProblems;
+
+namespace LeetCode.Tests.StringProblemsTests
+{
+    [TestClass]
+    public class SubstringwithConcatenationofAllWordsTests
+    {
+        [TestMethod]
+        public void Test_SubstringwithConcatenationofAllWords_1()
+        {
+            var o = new SubstringwithConcatenationofAllWords();
+            var actual = o.FindSubstring("barfoothefoobarman", new string[] { "foo", "bar" });
+
+            Assert.IsTrue(actual.SequenceEqual(new int[] { 0, 9 }));
+        }
+        [TestMethod]
+        public void Test_SubstringwithConcatenationofAllWords_2()
+        {
+            var o = new SubstringwithConcatenationofAllWords();
+            var actual = o.FindSubstring("wordgoodgoodgoodbestword", new string[] { "word", "good", "best", "good" });
+
+            Assert.IsTrue(actual.SequenceEqual(new int[] { 8 }));
+        }
+        [TestMethod]
+        public void Test_SubstringwithConcatenationofAllWords_3()
+        {
+            var o = new SubstringwithConcatenationofAllWords();
+            var actual = o.FindSubstring("mississippi", new string[] { "is" });
+
+            Assert.IsTrue(actual.SequenceEqual(new int[] { 1, 4 }));
+        }
+        [TestMethod]
+        public void Test_SubstringwithConcatenationofAllWords_TLE()
+        {
+            var o = new SubstringwithConcatenationofAllWords();
+            var actual = o.FindSubstring("pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel",
+                new string[] { "dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj", "lnle", "sefg", "vimw", "bxcb" });
+
+            Assert.IsTrue(actual.SequenceEqual(new int[] { 935 }));
+
+        }
+    }
+}
