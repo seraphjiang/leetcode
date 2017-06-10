70d17a3f55e0d26ddc241ff4004599812e0284c3
Huan Jiang <seraphjiang@gmail.com>
2017-06-09 17:51:07 -0700

    add hankrank and Large island problem, test case 14 will cause stack overflow or out of memory

---
+++ HackerRank.Problems/LargestIsland.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Problems
+{
+    /// <summary>
+    /// https://www.hackerrank.com/contests/iit-guwahati-placement-preparation-test-02/challenges/largest-island
+    /// You are given a map in the form of a rectangular grid. In this map, a 1 represents a piece of land and 0 represents water. A group of connected 1's forms an island. Two 1's are connected if they are adjacent on the grid (You can consider up, down, left, right as well as diagonal points to be adjacent). Your job is to find the area of the largest island.
+    /// </summary>
+    public class LargestIsland
+    {
+        static string[] islands;
+        static bool[,] visitied;
+        static int n;
+        static int m;
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            //var line1 = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var tr = new StreamReader("LargestIsland.txt");
+            var line1 = tr.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            n = int.Parse(line1[0]);
+            m = int.Parse(line1[1]);
+
+            islands = new string[n];
+            visitied = new bool[n, m];
+            for (var i = 0; i < n; i++)
+            {
+                //var lines = Console.ReadLine();
+                var lines = tr.ReadLine();
+                islands[i] = lines;
+            }
+            var max = 0;
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    if (visitied[i, j] || islands[i][j] == '0') continue;
+                    max = Math.Max(max, bfs(i,j));
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+
+        static int bfs(int x, int y)
+        {
+            if (visitied[x, y]) return 0;
+
+            visitied[x, y] = true;
+            var directions = new int[,]{
+                {1,0}, {-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}
+            };
+            var count = 0;
+            var q = new Queue<Tuple<int, int>>();
+            q.Enqueue(Tuple.Create(x, y));
+            while (q.Count > 0)
+            {
+                var node = q.Dequeue();
+                count++;
+                var x0 = node.Item1;
+                var y0 = node.Item2;
+                for (var i = 0; i < 8; i++)
+                {
+                    var r = x0 + directions[i, 0];
+                    var c = y0 + directions[i, 1];
+
+                    if (r < 0 || r >= n || c < 0 || c >= m || visitied[r, c] || islands[r][c] == '0') continue;
+
+                    q.Enqueue(Tuple.Create(r, c));
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
