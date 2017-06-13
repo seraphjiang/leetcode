3662428a6f72e23bce1770e2da130709a64a7ba1
Huan Jiang <seraphjiang@gmail.com>
2017-06-12 20:38:45 -0700

    Basic Calculator II

---
+++ LeetCode.Problems/StringProblems/BasicCalculatorII.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.StringProblems
+{
+    public class BasicCalculatorII
+    {
+        public const string nums = "0123456789";
+
+        public int Calculate(string s)
+        {
+            var total = 0;
+            var sign = 1;
+            var i = 0;
+            var num = GetNextNum(s, ref i);
+
+            while (i < s.Length)
+            {
+                var c = s[i];
+                ++i;
+                if (c == '+' || c == '-')
+                {
+                    total += sign * num;
+                    num = GetNextNum(s, ref i);
+                    sign = c == '+' ? 1 : -1;
+                }
+                else if (c == '*')
+                {
+                    num *= GetNextNum(s, ref i);
+                }
+                else if (c == '/')
+                {
+                    num /= GetNextNum(s, ref i);
+                }
+            }
+            total += sign * num;
+            return total;
+        }
+
+        public int GetNextNum(string s, ref int start)
+        {
+            var num = 0;
+            while (start < s.Length)
+            {
+                if (nums.Contains(s[start]))
+                {
+                    num = num * 10 + (s[start] - '0');
+                }
+                else if (s[start] != ' ')
+                {
+                    return num;
+                }
+                start++;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ LeetCode.Tests/StringProblemsTests/BasicCalculatorIITests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.StringProblems;
+
+namespace LeetCode.Tests.StringProblemsTests
+{
+    [TestClass]
+    public class BasicCalculatorIITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var o = new BasicCalculatorII();
+            var actual = o.Calculate("2*2");
+            Assert.Equals(-4, actual);
+        }
+    }
+}
