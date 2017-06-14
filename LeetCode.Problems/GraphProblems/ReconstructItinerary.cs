96ec510f0118312f242b34eba1432ff88ed58ac3
Huan Jiang <seraphjiang@gmail.com>
2017-06-14 11:26:33 -0700

    reconstruct iternaray

---
+++ LeetCode.Problems/GraphProblems/ReconstructItinerary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.GraphProblems
+{
+    public class ReconstructItinerary
+    {
+        public IList<string> FindItinerary(string[,] tickets)
+        {
+            var path = new List<string>();
+            var dict = new Dictionary<string, List<string>>();
+            for (var i = 0; i < tickets.GetLength(0); i++)
+            {
+                if (!dict.ContainsKey(tickets[i, 0])) dict[tickets[i, 0]] = new List<string>();
+                dict[tickets[i, 0]].Add(tickets[i, 1]);
+            }
+
+            foreach(var val in dict.Values)
+            {
+                val.Sort(); 
+            }
+            Dfs("JFK", dict, path);
+            return path;
+        }
+
+        public void Dfs(string from, Dictionary<string, List<string>> tickets, List<string> path)
+        {
+            if (tickets.ContainsKey(from))
+            {
+                var arrivals = new List<string>(tickets[from]);
+                
+                while(tickets[from].Count>0)
+                {
+                    var p = tickets[from][0];
+                    tickets[from].RemoveAt(0);
+                    Dfs(p, tickets, path);
+                }
+            }
+
+            path.Insert(0, from);
+        }
+    }
+}
---
+++ LeetCode.Tests/GraphProblemsTests/ReconstructItineraryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.GraphProblems;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.GraphProblemsTests
+{
+    [TestClass]
+    public class ReconstructItineraryTest
+    {
+        [TestMethod]
+        public void Test_ReconstructItinerary1()
+        {
+            var o = new ReconstructItinerary();
+            var input = new string[,]
+            {
+                { "JFK", "SFO"},{"JFK", "ATL"},{"SFO", "ATL"},{"ATL", "JFK"},{"ATL", "SFO"}
+            };
+            var expected = new List<string> { "JFK", "ATL", "JFK", "SFO", "ATL", "SFO" };
+            Assert.IsTrue(expected.SequenceEqual(o.FindItinerary(input)));
+        }
+    }
+}
