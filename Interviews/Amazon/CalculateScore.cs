207e1179b3885a16895d8d2243784d7e1f6648a8
Huan Jiang <seraphjiang@gmail.com>
2017-06-14 12:26:13 -0700

    two code test question

---
+++ Interviews/Amazon/CalculateScore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviews.Amazon
+{
+    public class CalculateScore
+    {
+        static void Main(string[] args)
+        {
+            var ret = totalScore(new string[] { "5", "-2", "4", "Z", "X", "9", "+", "+" }, 8);
+        }
+        // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+        public static int totalScore(string[] blocks, int n)
+        {
+            if (n == 0) return 0;
+            var stack = new Stack<int>();
+            stack.Push(0);
+            for (var i = 0; i < n; i++)
+            {
+                if (blocks[i] == "X")
+                {
+                    var p = stack.Count > 0 ? stack.Peek() : 0;
+                    stack.Push(p * 2);
+                }
+                else if (blocks[i] == "+")
+                {
+                    var p2 = stack.Count > 0 ? stack.Pop() : 0;
+                    var p1 = stack.Count > 0 ? stack.Pop() : 0;
+                    stack.Push(p1);
+                    stack.Push(p2);
+                    stack.Push(p1 + p2);
+                }
+                else if (blocks[i] == "Z")
+                {
+                    if (stack.Count > 0)
+                        stack.Pop();
+                }
+                else
+                {
+                    stack.Push(int.Parse(blocks[i]));
+                }
+            }
+
+            var total = 0;
+            while (stack.Count > 0)
+            {
+                total += stack.Pop();
+            }
+
+            return total;
+        }        // METHOD SIGNATURE ENDS
+    }
+}
---
+++ Interviews/Amazon/FindAllLunchs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviews.Amazon
+{
+    public class Class2
+    {
+        public static string[,] matchLunches(string[,] lunchMenuPairs,
+                                                string[,] teamCuisinePreference)
+        {
+            // WRITE YOUR CODE HERE
+            var list = new List<string[]>();
+            var m = lunchMenuPairs.GetLength(0);
+            var dict = new Dictionary<string, List<string>>();
+            for (var i = 0; i < m; ++i)
+            {
+                if (!dict.ContainsKey(lunchMenuPairs[i, 1])) dict[lunchMenuPairs[i, 1]] = new List<string>();
+
+                dict[lunchMenuPairs[i, 1]].Add(lunchMenuPairs[i, 0]);
+            }
+            var n = teamCuisinePreference.GetLength(0);
+            for (var i = 0; i < n; ++i)
+            {
+                var user = teamCuisinePreference[i, 0];
+                var cuisine = teamCuisinePreference[i, 1];
+                if (cuisine == "*")
+                {
+                    for (var j = 0; j < m; ++j)
+                    {
+                        list.Add(new string[] { user, lunchMenuPairs[j, 0] });
+                    }
+                }
+                else
+                {
+                    if (dict.ContainsKey(cuisine))
+                    {
+                        var menus = dict[cuisine];
+                        foreach (var menu in menus)
+                        {
+                            list.Add(new string[] { user, menu });
+                        }
+                    }
+                }
+            }
+
+            var ret = new string[list.Count, 2];
+            for (var i = 0; i < list.Count; ++i)
+            {
+                ret[i, 0] = list[i][0];
+                ret[i, 1] = list[i][1];
+            }
+
+            return ret;
+        }
+    }
+}
