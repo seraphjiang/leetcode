3c0527e1841547b5bf8b750ca21e35b2fa804664
Huan Jiang <seraphjiang@gmail.com>
2017-06-17 18:05:56 -0700

    Add QTC interview question solution and layout class for week contest 37

---
+++ Interviews/QTC/WaterFallShortestPath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviews.QTC
+{
+    public class WaterFallShortestPath
+    {
+        public class Point
+        {
+            public int X;
+            public int Y;
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        public class Slab
+        {
+            public int X;
+            public int Y;
+            public int Length;
+        }
+        public int GetPath(Point start, Slab[] slabs)
+        {
+            Array.Sort(slabs, (a, b) => { return b.Y.CompareTo(a.Y); });
+            return dfs(start, slabs, 0, 0) + start.Y;
+        }
+
+        public int dfs(Point start, Slab[] slabs, int level, int total)
+        {
+            if (level == slabs.Length)
+            {
+                return total;
+            }
+            var cur = slabs[level];
+            if ((slabs[level].X < start.X) && (slabs[level].X + slabs[level].Length < start.X))
+            {
+                var left = start.X - cur.X + dfs(new Point(cur.X, cur.Y), slabs, level + 1, total + (start.X - cur.X));
+                var right = cur.X + cur.Length - start.X + dfs(new Point(cur.X + cur.Length, cur.Y), slabs, level + 1, total + (cur.X + cur.Length - start.X));
+
+                return Math.Min(left, right);
+            }
+            
+            return dfs(start, slabs, level + 1, total);
+        }
+    }
+}
---
+++ LeetCode.Tests/ContestTests/Week37Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.Contests.Week37;
+
+namespace LeetCode.Tests.ContestTests
+{
+    [TestClass]
+    public class Week37Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var o = new Class1();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var o = new Class2();
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var o = new Class3();
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var o = new Class4();
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+    }
+}
