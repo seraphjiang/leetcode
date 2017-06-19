cd5af6fc784e21c26c8d45db83ffcdf3573af7fd
Huan Jiang <seraphjiang@gmail.com>
2017-06-19 15:53:24 -0700

    jump ii with gready as well as BFS

---
+++ LeetCode.Problems/DynamicProgrammingProblems/JumpGameII.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.DynamicProgrammingProblems
+{
+    public class JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length < 2) return 0;
+            var left = 0; var i = 0; var max = 0;
+            var level = 0; var next = 0;
+            while (max - i + 1 > 0) // nodes count of current level>0
+            {
+                level++;
+                for (; i <= max; i++)
+                {
+                    next = Math.Max(next, i + nums[i]); // traversal current level, update mac reach of next level
+                    if (next >= nums.Length - 1) return level;// if last element is in level+1, retrn jump = level;
+                }
+                max = next;
+            }
+
+            return 0;
+        }
+
+        public int Jump_Greedy(int[] nums)
+        {
+            int jumps = 0, curEnd = 0, curFarthest = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                curFarthest = Math.Max(curFarthest, i + nums[i]);
+                if (i == curEnd)
+                {
+                    jumps++;
+                    curEnd = curFarthest;
+                }
+            }
+            return jumps;
+        }
+
+        /// <summary>
+        /// 80/92
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int JumpTLEWithVisitied(int[] nums)
+        {
+            if (nums.Length <= 1) return 0;
+            var level = 0;
+            var max = 0;
+            var i = 0;
+            var q = new Queue<int>();
+            q.Enqueue(i++);
+            var visitied = new bool[nums.Length];
+            visitied[0] = true;
+
+            while (q.Count > 0)
+            {
+                var count = q.Count;
+                var small = i;
+                for (var j = 0; j < count; j++)
+                {
+                    var p = q.Dequeue();
+                    var right = p + nums[p];
+                    max = Math.Max(right, max);
+
+                    if (max >= nums.Length - 1)
+                    {
+                        return level + 1;
+                    }
+
+                    for (var k = 1; k <= nums[p]; k++)
+                    {
+                        if (visitied[p + k]) continue;
+                        q.Enqueue(p + k);
+                        visitied[p + k] = true;
+                    }
+                }
+                level++;
+            }
+
+            return level;
+        }
+        public int JumpWrong(int[] nums)
+        {
+            if (nums.Length == 1) return 0;
+            var left = 0;
+            var right = 0;
+            var min = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (right < i + nums[i])
+                {
+                    min++;
+                    left = right;
+                    right = i + nums[i]; // left is most left with current jump
+
+                }
+
+                if (right >= nums.Length - 1)
+                {
+                    return min;
+                }
+            }
+
+            return min;
+        }
+
+        /// <summary>
+        /// 91/92 only fail last input
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int JumpTLE(int[] nums)
+        {
+            if (nums.Length == 1) return 0;
+            var max = 0;
+            var dp = new int[nums.Length];
+
+            for (var i = 1; i < nums.Length; i++) dp[i] = nums.Length;
+            dp[0] = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var mostright = i + nums[i];
+                max = Math.Max(max, mostright);
+                if (max >= nums.Length - 1) return i == 0 ? 0 : dp[i] + 1;
+
+                for (var j = i + 1; j <= max; j++)
+                {
+                    dp[j] = Math.Min(dp[j], dp[i] + 1);
+                }
+            }
+
+            return dp[nums.Length - 1];
+        }
+    }
+}
---
+++ LeetCode.Tests/DPProblemsTests/JumpGameIITests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.DynamicProgrammingProblems;
+
+namespace LeetCode.Tests.DPProblemsTests
+{
+    [TestClass]
+    public class JumpGameIITests
+    {
+        [TestMethod]
+        public void Test_JumpGameII_1()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(2, o.Jump(new int[] { 1, 2, 3 }));
+        }
+        [TestMethod]
+        public void Test_JumpGameII_2()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(0, o.Jump(new int[] { 1 }));
+        }
+        [TestMethod]
+        public void Test_JumpGameII_3()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(1, o.Jump(new int[] { 1, 2 }));
+        }
+        [TestMethod]
+        public void Test_JumpGameII_4()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(2, o.Jump(new int[] { 7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3 }));
+        }
+        [TestMethod]
+        public void Test_JumpGameII_5()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(3, o.Jump(new int[] { 1, 2, 1, 1, 1 }));
+        }
+        [TestMethod]
+        public void Test_JumpGameII_6()
+        {
+            var o = new JumpGameII();
+            Assert.AreEqual(4, o.Jump(new int[] { 6, 2, 6, 1, 7, 9, 3, 5, 3, 7, 2, 8, 9, 4, 7, 7, 2, 2, 8, 4, 6, 6, 1, 3 }));
+        }
+    }
+}
