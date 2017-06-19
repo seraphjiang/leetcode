9023abebd8c189cead73fb91c3d88bfe064b750d
Huan Jiang <seraphjiang@gmail.com>
2017-06-19 11:41:55 -0700

    sudoku solver backtracking O(9^m)

---
+++ LeetCode.Problems/GraphProblems/SudokuSolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.GraphProblems
+{
+    public class SudokuSolver
+    {
+        public void SolveSudoku(char[,] board)
+        {
+            Solve(board, 0, 0);
+        }
+
+        public bool Solve(char[,] board, int x, int y)
+        {
+            if (x == 8 && y == 8 && board[x, y] != '.') return IsValid(board, x, y);
+
+            if (board[x, y] == '.')
+            {
+                for (var i = 1; i < 10; i++)
+                {
+                    board[x, y] = (char)('0' + i);
+                    if (Solve(board, x, y))
+                    {
+                        return true;
+                    }
+                    board[x, y] = '.';
+                }
+            }
+            else
+            {
+                if (!IsValid(board, x, y)) return false;
+                if (y < 8)
+                {
+                    if (Solve(board, x, y + 1)) return true;
+                }
+                else if (y == 8 && x < 8)
+                {
+                    if (Solve(board, x + 1, 0)) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsValid(char[,] board, int x, int y)
+        {
+            var r0 = (x / 3) * 3;
+            var c0 = (y / 3) * 3;
+            if (board[x, y] == '.') return false;
+            for (var i = 0; i < 9; i++)
+            {
+                if (i != y && board[x, i] == board[x, y]) return false;
+                if (i != x && board[i, y] == board[x, y]) return false;
+
+                var r = r0 + i / 3;
+                var c = c0 + i % 3;
+
+                if (r != x && c != y && board[r, c] == board[x, y]) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Tests/GraphProblemsTests/SudokuSolverTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.GraphProblems;
+using System.Diagnostics;
+
+namespace LeetCode.Tests.GraphProblemsTests
+{
+    [TestClass]
+    public class SudokuSolverTests
+    {
+        [TestMethod]
+        public void Test_SudokuSolver_1()
+        {
+            var input = new char[][]
+            {
+                "..9748...".ToCharArray(),
+                "7........".ToCharArray(),
+                ".2.1.9...".ToCharArray(),
+                "..7...24.".ToCharArray(),
+                ".64.1.59.".ToCharArray(),
+                ".98...3..".ToCharArray(),
+                "...8.3.2.".ToCharArray(),
+                "........6".ToCharArray(),
+                "...2759..".ToCharArray()
+            };
+
+            var board = new char[9, 9];
+            for(var i = 0; i < 9; i++)
+            {
+                for(var j = 0; j < 9; j++)
+                {
+                    board[i, j] = input[i][j];
+                }
+            }
+            var o = new SudokuSolver();
+            o.SolveSudoku(board);
+
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    Debug.Write(board[i, j]);
+                }
+                Debug.WriteLine("");
+            }
+        }
+    }
+}
