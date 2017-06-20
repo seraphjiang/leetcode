1df232725211b621a5be8ec2cc48ee8c19e624e5
Huan Jiang <seraphjiang@gmail.com>
2017-06-20 16:07:42 -0700

    Text Justification a ugly naive implementation

---
+++ LeetCode.Problems/StringProblems/TextJustification.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.StringProblems
+{
+    public class TextJustification
+    {
+        public IList<string> FullJustify(string[] words, int maxWidth)
+        {
+            var ret = new List<string>();
+
+            var level = new List<string>();
+            var len = 0;
+            for (var i = 0; i < words.Length; ++i)
+            {
+                // total word len + num of space + current word
+                if (len + level.Count + words[i].Length > maxWidth) // exceed
+                {
+                    var spaces = maxWidth - len;
+                    var sp = level.Count == 1 ? 0 : (spaces) / (level.Count - 1);
+                    var addition = spaces - sp * (level.Count - 1);
+                    var sb = new StringBuilder();
+                    sb.Append(level[0]);
+                    for (var j = 1; j < level.Count; j++)
+                    {
+                        if (addition > 0)
+                        {
+                            sb.Append(' ', sp + 1);
+                            addition--;
+                        }
+                        else
+                        {
+                            sb.Append(' ', sp);
+                        }
+                        sb.Append(level[j]);
+                    }
+                    if(sb.Length< maxWidth)
+                    {
+                        sb.Append(new string(' ', maxWidth - sb.Length));
+                    }
+                    ret.Add(sb.ToString());
+                    level = new List<string> { words[i] };
+                    len = words[i].Length;
+                }
+                else
+                {
+                    len += words[i].Length;
+                    level.Add(words[i]);
+                }
+            }
+            var last = string.Join(" ", level);
+            if(last.Length < maxWidth)
+            {
+                ret.Add(last + new string(' ', maxWidth - last.Length));
+            }
+            else
+            {
+                ret.Add(last);
+            }
+            
+            return ret;
+        }
+    }
+}
---
+++ LeetCode.Tests/StringProblemsTests/TextJustificationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.StringProblems;
+
+namespace LeetCode.Tests.StringProblemsTests
+{
+    [TestClass]
+    public class TextJustificationTests
+    {
+        [TestMethod]
+        public void Test_TextJustification_1()
+        {
+            var input = new string[] { "This", "is", "an", "example", "of", "text", "justification." };
+            var o = new TextJustification();
+            var act = o.FullJustify(input, 16);
+            Assert.AreEqual(3, act.Count);
+        }
+        [TestMethod]
+        public void Test_TextJustification_2()
+        {
+            var input = new string[] { "a", "c", "c"};
+            var o = new TextJustification();
+            var act = o.FullJustify(input, 1);
+            Assert.AreEqual(3, act.Count);
+        }
+    }
+}
