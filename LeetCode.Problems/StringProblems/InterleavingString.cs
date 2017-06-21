92b738dcacfb9771f87d341a6cab76a7a35900a9
Huan Jiang <seraphjiang@gmail.com>
2017-06-21 11:27:28 -0700

    DP solution for interleaving string

--- LeetCode.Problems/StringProblems/InterleavingString.cs
+++ LeetCode.Problems/StringProblems/InterleavingString.cs
@@ -13,6 +13,27 @@ namespace LeetCode.Problems.StringProblems
             if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2) && string.IsNullOrEmpty(s3)) return true;
             if (string.IsNullOrEmpty(s3)) return false;
             if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2)) return s3.Equals(s1 + s2);
+            if (s1.Length + s2.Length != s3.Length) return false;
+            var dp = new bool[s1.Length, s2.Length];
+            for (var i = 0; i <= s1.Length; ++i)
+            {
+                for (var j = 0; j <= s2.Length; ++j)
+                {
+                    if (i == 0 && j == 0) dp[i, j] = true;
+                    else if (i == 0) dp[i, j] = dp[i, j - 1] && s2[j - 1] == s3[i + j - 1];
+                    else if (j == 0) dp[i, j] = dp[i - 1, j] && s1[i - 1] == s3[i + j - 1];
+                    else dp[i, j] = (dp[i, j - 1] && s2[j - 1] == s3[i + j - 1]) || (dp[i, j] = dp[i - 1, j] && s1[i - 1] == s3[i + j - 1]);
+                }
+            }
+
+            return dp[s1.Length, s2.Length];
+        }
+
+        public bool IsInterleaveDFS(string s1, string s2, string s3)
+        {
+            if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2) && string.IsNullOrEmpty(s3)) return true;
+            if (string.IsNullOrEmpty(s3)) return false;
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2)) return s3.Equals(s1 + s2);
 
             if (s1.Length + s2.Length != s3.Length) return false;
             if (s3.Equals(s1 + s2)) return true;
