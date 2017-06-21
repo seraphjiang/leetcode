a11bd14afc6d2c62e508ed6a19a877d6cbd47bea
Huan Jiang <seraphjiang@gmail.com>
2017-06-20 17:28:59 -0700

    dp solution for edit distance O(m*n) time and space complexity

---
+++ LeetCode.Problems/DynamicProgrammingProblems/EditDistance.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.DynamicProgrammingProblems
+{
+    public class EditDistance
+    {
+        public int MinDistance(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1)) return string.IsNullOrEmpty(word2) ? 0 : word2.Length;
+            if (string.IsNullOrEmpty(word2)) return word1.Length;
+
+            var dp = new int[word1.Length + 1, word2.Length + 1];
+
+            for (var i = 1; i <= word1.Length; i++)
+            {
+                dp[i, 0] = i;
+            }
+            for (var i = 1; i <= word2.Length; i++)
+            {
+                dp[0, i] = i;
+            }
+
+            for (var i = 1; i <= word1.Length; ++i)
+            {
+                for (var j = 1; j <= word2.Length; ++j)
+                {
+                    dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i, j - 1], dp[i - 1, j]));
+                    if (word1[i - 1] == word2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i, j - 1], dp[i - 1, j])) + 1;
+                    }
+                }
+            }
+
+            return dp[word1.Length, word2.Length];
+        }
+    }
+}
---
+++ LeetCode.Tests/DPProblemsTests/EditDistanceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.DynamicProgrammingProblems;
+
+namespace LeetCode.Tests.DPProblemsTests
+{
+    [TestClass]
+    public class EditDistanceTests
+    {
+        [TestMethod]
+        public void Test_EditDistance_1()
+        {
+            var o = new EditDistance();
+            Assert.AreEqual(0, o.MinDistance("a", "a"));
+            Assert.AreEqual(1, o.MinDistance("a", "aa"));
+            Assert.AreEqual(1, o.MinDistance("a", "b"));
+        }
+    }
+}
