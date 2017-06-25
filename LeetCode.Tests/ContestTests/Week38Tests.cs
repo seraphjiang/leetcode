ae62c8dc2374ab58fefa5546180b12c263371fc1
Huan Jiang <seraphjiang@gmail.com>
2017-06-24 18:03:07 -0700

    layout files for contest

---
+++ LeetCode.Tests/ContestTests/Week38Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.Contests.Week38;
+
+namespace LeetCode.Tests.ContestTests
+{
+    [TestClass]
+    public class Week38Tests
+    {
+        [TestMethod]
+        [TestProperty("WeeklyContest", "38")]
+
+        public void TestMethod1()
+        {
+            var o = new Class1();
+            var exp = 1;
+
+            Assert.AreEqual(exp, exp);
+        }
+
+        [TestMethod]
+        [TestProperty("WeeklyContest", "38")]
+
+        public void TestMethod2()
+        {
+            var o = new Class2();
+            var exp = 1;
+
+            Assert.AreEqual(exp, exp);
+        }
+        [TestMethod]
+        [TestProperty("WeeklyContest", "38")]
+
+        public void TestMethod3()
+        {
+            var o = new Class3();
+            var exp = 1;
+
+            Assert.AreEqual(exp, exp);
+        }
+        [TestMethod]
+        [TestProperty("WeeklyContest", "38")]
+
+        public void TestMethod4()
+        {
+            var o = new Class4();
+            var exp = 1;
+
+            Assert.AreEqual(exp, exp);
+        }
+    }
+}
