3e11371798fcb0dce96d3fe604e14489af4e9093
Huan Jiang <seraphjiang@gmail.com>
2017-06-25 14:32:51 -0700

    week contest     public int MaximumProduct(int[] nums) {

---
+++ LeetCode.Problems/Contests/Week38/MaximumProductofThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Contests.Week38
+{
+    public class MaximumProductofThreeNumbers
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+
+            var ret = nums[nums.Length - 1];
+
+            ret *= Math.Max(nums[0] * nums[1], nums[nums.Length - 2] * nums[nums.Length - 3]);
+
+            return ret;
+        }
+    }
+}
