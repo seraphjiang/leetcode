832dc45c3e4ddcb02e80f7350faddd09f0786f61
Huan Jiang <seraphjiang@gmail.com>
2017-06-26 11:58:11 -0700

    Decrypt Message from pramp.com

---
+++ LeetCode.Tests/OtherSites/PrampTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OtherSites.Problems.Pramp;
+
+namespace LeetCode.Tests.OtherSites
+{
+    [TestClass]
+    public class PrampTests
+    {
+        [TestMethod]
+        [TestProperty("pramp.com", "DecryptMessage")]
+        public void Test_Pramp_DecryptMessage_1()
+        {
+            var o = new DecryptMessage();
+            Assert.AreEqual("crime", o.Decrypt("dnotq"));
+        }
+    }
+}
---
+++ OtherSites.Problems/Pramp/DecryptMessage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherSites.Problems.Pramp
+{
+    public class DecryptMessage
+    {
+        public string Encrypt(string word)
+        {
+            //'a' .. 'z' 97..122
+            // convert to ascii
+            // add 1 to first
+            // keep add with previous
+            // subtract 26 till ascii
+            return word;
+        }
+
+
+        public string Decrypt(string word)
+        {
+            var sb = new StringBuilder();
+            var pre = 1;
+            for(var i = 0; i < word.Length; ++i)
+            {
+                var c = (int)word[i];
+                c = c - pre;
+                while (c < 'a')
+                {
+                    c += 26;
+                }
+
+                sb.Append((char)c);
+                pre += c;
+            }
+            return sb.ToString();
+        }
+
+        public string DecryptWrong(string word)
+        {
+            // add back 26
+            // c[n] = c[n] - c[n-1]
+            // first calc -1
+            // 
+
+            var chars = word.ToCharArray();
+            var ascii = new int[word.Length];
+            for (var i = 0; i < word.Length; i++)
+            {
+                ascii[i] = (int)chars[i];
+            }
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (i == 0)
+                {
+                    ascii[i] = ascii[i] - 1;
+                }
+                else
+                {
+                    ascii[i] -= ascii[i - 1];
+
+                    while (!(ascii[i] >= 'a' && ascii[i] <= 'z'))
+                    {
+                        ascii[i] += 26;
+                    }
+                }
+            }
+            for (var i = 0; i < word.Length; i++)
+            {
+                chars[i] = (char)ascii[i];
+            }
+
+            var s = new string(chars);
+            return s;
+        }
+    }
+}
