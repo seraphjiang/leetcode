b845ffe42cb24b7526a20ad6238391b682117f04
Huan Jiang <seraphjiang@gmail.com>
2017-06-26 21:32:23 -0700

    back tracking to solve expression add operators

---
+++ LeetCode.Problems/BacktrackingProblems/ExpressionAddOperators.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.BacktrackingProblems
+{
+    public class ExpressionAddOperators
+    {
+        public IList<string> AddOperators(string num, int target)
+        {
+            var res = new List<string>();
+            if (string.IsNullOrEmpty(num)) return res;
+
+            help(num, 0, 0, 0, target, "", res);
+
+            return res;
+        }
+
+        public void help(string num, int index, long eval, long pre, int target, string path, List<string> res)
+        {
+            if (index == num.Length)
+            {
+                if (eval == target) res.Add(path);
+                return;
+            }
+
+            for (var i = index; i < num.Length; i++)
+            {
+                //if (i != index && num[i] == '0') break;
+                if (i != index && num[index] == '0') break;
+
+                long cur = long.Parse(num.Substring(index, i - index + 1));
+                if (index == 0)
+                {
+                    help(num, i + 1, cur, cur, target, cur.ToString(), res);
+                }
+                else
+                {
+                    help(num, i + 1, eval + cur, cur, target, path + "+" + cur.ToString(), res);
+                    help(num, i + 1, eval - cur, -cur, target, path + "-" + cur.ToString(), res);
+                    help(num, i + 1, eval - pre + pre * cur, pre * cur, target, path + "*" + cur.ToString(), res);
+                }
+            }
+
+        }
+    }
+}
---
+++ LeetCode.Tests/BacktrackingProblemsTests/ExpressionAddOperatorsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.BacktrackingProblems;
+
+namespace LeetCode.Tests.BacktrackingProblemsTests
+{
+    [TestClass]
+    public class ExpressionAddOperatorsTests
+    {
+        [TestMethod]
+        public void Test_ExpressionAddOperators_1()
+        {
+            var o = new ExpressionAddOperators();
+            Assert.AreEqual(new string[] { "1*0+5", "10-5" }, o.AddOperators("105", 5));
+        }
+    }
+}
