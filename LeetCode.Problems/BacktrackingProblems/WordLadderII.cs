bc3a363fff7b91f1c42db252862b483db4912352
Huan Jiang <seraphjiang@gmail.com>
2017-06-29 22:29:30 -0700

    word ladder II add one LTE solution and AC solution

--- LeetCode.Problems/BacktrackingProblems/WordLadderII.cs
+++ LeetCode.Problems/BacktrackingProblems/WordLadderII.cs
@@ -10,12 +10,187 @@ namespace LeetCode.Problems.BacktrackingProblems
     {
         public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
         {
+            var result = new List<IList<string>>();
+            if (string.IsNullOrEmpty(beginWord) || string.IsNullOrEmpty(endWord) || wordList == null || !wordList.Any())
+                return result;
+            var neighbours = new Dictionary<string, List<string>>();
+            var set = new HashSet<string>(wordList);
+            var distance = new Dictionary<string, int>();
+            distance[beginWord] = 0;
+            set.Add(beginWord);
+            Bfs(beginWord, endWord, neighbours, distance, set);
+            Dfs(beginWord, endWord, neighbours, distance, result, new List<string>());
+            return result;
+        }
+
+        private void Bfs(string begin, string end, Dictionary<string, List<string>> neighbours, Dictionary<string, int> distance, HashSet<string> set)
+        {
+            var queue = new Queue<string>();
+            var isEnd = false;
+            queue.Enqueue(begin);
+            while (queue.Any() && !isEnd)
+            {
+                var t = new Queue<string>();
+                while (queue.Any())
+                {
+                    var node = queue.Dequeue();
+                    var newNeighbours = GetNeighbours(node, set);
+                    foreach (var newWord in newNeighbours)
+                    {
+                        if (!neighbours.ContainsKey(node))
+                        {
+                            neighbours[node] = new List<string>();
+                        }
+                        neighbours[node].Add(newWord);
+                        if (!distance.ContainsKey(newWord))
+                        {
+                            distance[newWord] = distance[node] + 1;
+                            if (newWord == end)
+                            {
+                                isEnd = true;
+                                break;
+                            }
+                            else
+                            {
+                                t.Enqueue(newWord);
+                            }
+                        }
+                    }
+                }
+                queue = t;
+            }
+        }
+
+        private void Dfs(string start, string end, Dictionary<string, List<string>> neighbours, Dictionary<string, int> distance, IList<IList<string>> result, IList<string> temp)
+        {
+            temp.Add(start);
+            if (start == end)
+            {
+                result.Add(new List<string>(temp));
+            }
+            else
+            {
+                if (neighbours.ContainsKey(start))
+                {
+                    foreach (var newWord in neighbours[start])
+                    {
+                        if (distance[newWord] == distance[start] + 1)
+                        {
+                            Dfs(newWord, end, neighbours, distance, result, temp);
+                        }
+                    }
+                }
+
+            }
+
+            temp.RemoveAt(temp.Count - 1);
+        }
+
+
+        private IList<string> GetNeighbours(string word, HashSet<string> set)
+        {
+            var result = new List<string>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                var tempWord = word.ToCharArray();
+                var cTemp = tempWord[i];
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    if (c != cTemp)
+                    {
+                        tempWord[i] = c;
+                        var newString = new string(tempWord);
+                        if (set.Contains(newString))
+                        {
+                            result.Add(newString);
+                        }
+                        tempWord[i] = cTemp;
+                    }
+
+                }
+            }
+            return result;
+        }
+        public IList<IList<string>> FindLaddersWrong(string beginWord, string endWord, IList<string> wordList)
+        {
+            var graph = new Dictionary<string, List<string>>();
+            var res = new List<IList<string>>();
+            var set = new HashSet<string>(wordList);
+
+            if (!set.Contains(endWord)) return res;
+
+            var q = new Queue<string>();
+            q.Enqueue(beginWord);
+            var level = 0;
+            var found = false;
+            while (q.Count > 0)
+            {
+                var count = q.Count;
+                level++;
+                for (var i = 0; i < count; i++)
+                {
+                    var s = q.Dequeue();
+                    if (s.Equals(endWord)) found = true;
+                    if (!graph.ContainsKey(s)) graph[s] = new List<string>();
+
+                    for (var j = 0; j < s.Length; j++)
+                    {
+                        var arr = s.ToCharArray();
+                        var c = arr[j];
+                        for (var k = 0 + 'a'; k <= 0 + 'z'; k++)
+                        {
+                            if (c == k) continue;
+                            arr[j] = (char)k;
+                            var newStr = new string(arr);
+                            if (set.Contains(newStr))
+                            {
+                                graph[s].Add(newStr);
+                                q.Enqueue(newStr);
+                            }
+                        }
+                        arr[j] = c; //key point 2;
+                    }
+                }
+
+                if (found) break;
+            }
+
+            DfsWrong(res, set ,graph, beginWord, endWord, level, new List<string> { beginWord });
+            return res;
+        }
+
+        public void DfsWrong(IList<IList<string>> res, HashSet<string> set, Dictionary<string, List<string>> graph, string begin, string end, int level, IList<string> path)
+        {
+
+            if (path.Count >= level)
+            {
+                if (path[path.Count - 1].Equals(end) && path.Count == level) res.Add(new List<string>(path));
+                return;
+            }
+
+            if (graph.ContainsKey(begin))
+            {
+                var nexts = graph[begin];
+                foreach (var next in nexts)
+                {
+                    path.Add(next);
+                    set.Remove(next);
+                    DfsWrong(res, set, graph, next, end, level, path);
+                    set.Add(next);
+                    path.RemoveAt(path.Count - 1);
+                }
+            }
+        }
+
+        public IList<IList<string>> FindLaddersTLE2(string beginWord, string endWord, IList<string> wordList)
+        {
             var list = new List<IList<string>>();
             var hs = new HashSet<string>(wordList);
-            if (!hs.Contains(endWord)) return list;
+            if (!hs.Contains(endWord)) return list; // keypoint 1
 
             var dict = new Dictionary<string, List<string>>();
-            wordList.Add(beginWord);
+            if(!hs.Contains(beginWord))
+                wordList.Add(beginWord);
 
             foreach (var s in wordList)
             {
@@ -25,6 +200,7 @@ namespace LeetCode.Problems.BacktrackingProblems
                     var o = sb[i];
                     foreach (var c in "abcdefghijklmnopqrstuvwxyz")
                     {
+                        if (c == o) continue; // key point 3
                         sb[i] = c;
                         var key = sb.ToString();
                         if (hs.Contains(key))
@@ -36,7 +212,7 @@ namespace LeetCode.Problems.BacktrackingProblems
                     sb[i] = o;
                 }
             }
-
+            hs.Remove(beginWord);
             Dfs(beginWord, endWord, hs, dict, new List<string> { beginWord }, list);
 
             return list;
---
+++ LeetCode.Tests/BacktrackingProblemsTests/WordLadderIITests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.BacktrackingProblems;
+using System.Collections.Generic;
+using System.Linq;
+namespace LeetCode.Tests.BacktrackingProblemsTests
+{
+    [TestClass]
+    public class WordLadderIITests
+    {
+        [TestMethod]
+        public void Test_WordLadderII_1()
+        {
+            var o = new WordLadderII();
+            var act = o.FindLadders("hit", "cog", new List<string> { "hot", "dot", "dog", "lot", "log", "cog" });
+        }
+
+        [TestMethod]
+        public void Test_WordLadderII_2()
+        {
+            var o = new WordLadderII();
+            var act = o.FindLadders("a", "c", new List<string> { "a", "b", "c" });
+            var list = new List<IList<string>> { new List<string> { "a", "c" } };
+            Assert.IsTrue(act.SequenceEqual(list));
+        }
+    }
+}
