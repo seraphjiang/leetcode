b81b53a402e85be9440d3bd9d1d51d717a0df4a5
Huan Jiang <seraphjiang@gmail.com>
2017-07-01 17:46:03 -0700

    layout contest files

---
+++ LeetCode.Problems/Contests/Week39/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Contests.Week39
+{
+    public class Class4
+    {
+    }
+}
---
+++ LeetCode.Tests/ContestTests/Week39Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems.Contests.Week39;
+using System.Collections.Generic;
+using Commons;
+namespace LeetCode.Tests.ContestTests
+{
+    [TestClass]
+    public class Week39Tests
+    {
+        [TestMethod]
+        [TestProperty("WeeklyContest", "39")]
+
+        public void Test_Contest_Week39_Problem1()
+        {
+            var o = new Class1();
+            var exp = 1;
+
+            Assert.AreEqual(exp, exp);
+        }
+
+        [TestMethod]
+        [TestProperty("WeeklyContest", "39")]
+
+        public void Test_Contest_Week39_Problem2()
+        {
+            var o = new Class2();
+        }
+        [TestMethod]
+        [TestProperty("WeeklyContest", "39")]
+
+        public void Test_Contest_Week39_Problem2_1()
+        {
+            var o = new Class2();
+
+        }
+
+        [TestMethod]
+        [TestProperty("WeeklyContest", "39")]
+
+        public void Test_Contest_Week39_Problem3()
+        {
+            var o = new Class3();
+        }
+     
+        [TestMethod]
+        [TestProperty("WeeklyContest", "39")]
+
+        public void Test_Contest_Week39_Problem4()
+        {
+            var o = new Class4();
+        }
+    }
+}
