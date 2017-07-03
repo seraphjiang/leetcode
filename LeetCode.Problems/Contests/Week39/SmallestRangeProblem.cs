c13b20167941810ef7c630e1c00553349d36d0cb
Huan Jiang <seraphjiang@gmail.com>
2017-07-03 11:01:34 -0700

    Palindrome Linked List iterative and recursive solution

--- LeetCode.Problems/Contests/Week39/SmallestRangeProblem.cs
+++ LeetCode.Problems/Contests/Week39/SmallestRangeProblem.cs
@@ -8,7 +8,6 @@ namespace LeetCode.Problems.Contests.Week39
 {
     public class SmallestRangeProblem
     {
-
         public int[] SmallestRange(IList<IList<int>> nums)
         {
             var start = -1;
---
+++ LeetCode.Problems/ListNodeProblems/PalindromeLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using LeetCode.DataStructure.LeetCodeDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.ListNodeProblems
+{
+    public class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+            var fast = head;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            if (fast != null)
+            {
+                slow = slow.next;
+            }
+
+            slow = Reverse(slow);
+
+            while (head != null && slow != null)
+            {
+                if (head.val != slow.val)
+                {
+                    return false;
+                }
+                head = head.next;
+                slow = slow.next;
+            }
+
+            return true;
+        }
+
+        public ListNode Reverse(ListNode head)
+        {
+            ListNode pre = null;
+            ListNode next;
+            while (head != null)
+            {
+                next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+            }
+
+            return pre;
+        }
+        public bool IsPalindromeRecursive(ListNode head)
+        {
+            return head == null || recurse(head, head) != null;
+        }
+
+        private ListNode recurse(ListNode node, ListNode head)
+        {
+            if (node == null) return head; // exit recusion when reach end of tail
+            ListNode res = recurse(node.next, head); // in recusion, node iterate from tail to head, res from head to tail.
+            if (res == null) return res;
+            else if (res.val == node.val) return (res.next == null ? res : res.next);   // return compare node(cur from tail) to head
+            else return null;
+        }
+    }
+}
