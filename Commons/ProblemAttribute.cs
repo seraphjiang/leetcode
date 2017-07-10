46718bc4ece3ef66afb37a79da8cd29348c6d9c9
Huan Jiang <seraphjiang@gmail.com>
2017-07-10 16:29:10 -0700

    wiggle sort

---
+++ Commons/ProblemAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons
+{
+    public enum Level
+    {
+        Easy,
+        Median,
+        Hard
+    }
+    [AttributeUsage(AttributeTargets.All)]
+    public class ProblemAttribute : Attribute
+    {
+        public string Company { get; set; }
+        public Level Level { get; set; }
+        public string[] Algorithms { get; set; }
+        public string[] DataStructure { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
---
+++ LeetCode.Problems/ArrayProblems/WiggleSortProblem.cs
@@ -0,0 +1,40 @@
+﻿using Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.ArrayProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/wiggle-sort/#/description
+    /// Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+    /// For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+    /// </summary>
+    [Problem(Company =  "Google" , Level = Level.Median)]
+    public class WiggleSortProblem
+    {
+        public void WiggleSort(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i != 0 && nums[i] > nums[i - 1]) Swap(nums, i, i - 1);
+                }
+                else
+                {
+                    if (nums[i - 1] > nums[i]) Swap(nums, i, i - 1);
+                }
+            }
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            var t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+    }
+}
