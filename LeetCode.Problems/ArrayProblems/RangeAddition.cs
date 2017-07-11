ebbc0bd79705d82857517773913836626b2505ba
Huan Jiang <seraphjiang@gmail.com>
2017-07-11 13:00:36 -0700

    RangeAddition O(n) with partial sum and mark board range

---
+++ LeetCode.Problems/ArrayProblems/RangeAddition.cs
@@ -0,0 +1,45 @@
+﻿using Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.ArrayProblems
+{
+    [Problem(Company = "Google", Level = Level.Median)]
+    public class RangeAddition
+    {
+        public int[] GetModifiedArray(int length, int[,] updates)
+        {
+            var res = new int[length];
+
+            for (var i = 0; i < updates.GetLength(0); i++)
+            {
+                res[updates[i, 0]] += updates[i, 2];
+                if (updates[i, 1] != res.Length - 1) res[updates[i, 1] + 1] -= updates[i, 2];
+            }
+
+            for (var i = 0; i < res.Length; i++)
+            {
+                res[i] += i == 0 ? 0 : res[i - 1];
+            }
+
+            return res;
+        }
+
+        public int[] GetModifiedArrayTLE(int length, int[,] updates)
+        {
+            var res = new int[length];
+            for (var i = 0; i < updates.GetLength(0); i++)
+            {
+                for (var j = updates[i, 0]; j <= updates[i, 1]; j++)
+                {
+                    res[j] += updates[i, 2];
+                }
+            }
+
+            return res;
+        }
+    }
+}
